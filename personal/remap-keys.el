94a1fbb3cd488cad6bfd62811cb0ded95324ba0e
David Martinez Barreiro <5133990+dmbarreiro@users.noreply.github.com>
2023-10-09 14:41:47 +0200

    remapping hyper, super, meta and window navigation
    adding default macos mapping for emacs legacy keys
    and easier window navigation with shift and direction
    keys

---
+++ personal/remap-keys.el
@@ -0,0 +1,4 @@
+;; set keys for Apple keyboard, for emacs in OS X
+(setq mac-command-modifier 'meta) ; make cmd key do Meta
+(setq mac-option-modifier 'super) ; make opt key do Super
+(setq ns-function-modifier 'hyper)  ; make Fn key do Hyper
