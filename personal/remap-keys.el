4ae237e278cce7578965d4e07d94138871bb490a
David Martinez Barreiro <5133990+dmbarreiro@users.noreply.github.com>
2023-11-21 14:20:47 +0100

    get back to default bindings for M and s keys

--- personal/remap-keys.el
+++ personal/remap-keys.el
@@ -1,4 +1,4 @@
 ;; set keys for Apple keyboard, for emacs in OS X
-(setq mac-command-modifier 'meta) ; make cmd key do Meta
-(setq mac-option-modifier 'super) ; make opt key do Super
+;(setq mac-option-modifier 'super) ; make opt key do Super
+(setq mac-command-modifier 'super) ; make cmd key do Super
 (setq ns-function-modifier 'hyper)  ; make Fn key do Hyper
